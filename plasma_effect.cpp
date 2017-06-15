74158926c137cb11670c5ab1667bf866d8d02402
Jan Losinski <losinski@wh2.tu-dresden.de>
2017-06-15 18:24:19 +0200

    Lower the cpu usage of the plasma effect
    According to perf this lowers the overhead the plasma effect
    contributes to the program from 11% to 8%. It also lowers the calls to
    sin() and cos();
    
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- plasma_effect.cpp
+++ plasma_effect.cpp
@@ -5,13 +5,21 @@ void PlasmaEffect::fill(EffectBuffer& buffer, const EffectState& state)
 {
     double time = state.time;
     const float PI = 3.141592f;
+
+    const float sin_time = sin(time);
+    const float cos_time = cos(time);
+    const float sin_minus_time = -sin_time;
+    const float cos_minus_time = cos_time;
+
+    const float centerX = buffer.width()/2.0f + buffer.width()/2.0f*sin_minus_time;
+    const float centerY = buffer.height()/2.0f + buffer.height()/2.0f*cos_minus_time;
+
+    const float blue	= (sin(time)+1.0f)/2.0f;
+
     for (int y=0; y<buffer.height(); y++) {
         for (int x=0; x<buffer.width(); x++) {
 
-            float color1 = (sin(dot(x+1.0f, sin(time), y+1.0f, cos(time))*0.6f+time)+1.0f)/2.0f;
-            float centerX = buffer.width()/2.0f + buffer.width()/2.0f*sin(-time);
-
-            float centerY = buffer.height()/2.0f + buffer.height()/2.0f*cos(-time);
+            float color1 = (sin(dot(x+1.0f, sin_time, y+1.0f, cos_time)*0.6f+time)+1.0f)/2.0f;
 
             float color2 = (cos(length(x - centerX, y - centerY)*0.3f)+1.0f)/2.0f;
 
@@ -19,9 +27,8 @@ void PlasmaEffect::fill(EffectBuffer& buffer, const EffectState& state)
 
             float red	= (cos(PI*color/0.5f+time)+1.0f)/2.0f;
             float green	= (sin(PI*color/0.5f+time)+1.0f)/2.0f;
-            float blue	= (sin(time)+1.0f)/2.0f;
 
-            buffer[y * buffer.width() + x] = Color3(red, green, blue);
+            buffer.set(x, y, Color3(red, green, blue));
         }
     }
 }
