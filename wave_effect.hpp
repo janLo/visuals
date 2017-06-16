3e606b014f35e7352a6e71e48e6a78d0cfb87f6b
Jan Losinski <losinski@wh2.tu-dresden.de>
2017-06-16 21:36:03 +0200

    Make inverse wave possible
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- wave_effect.hpp
+++ wave_effect.hpp
@@ -8,9 +8,10 @@ class TopDownWaveEffect : public Effect
 {
     double m_start = -100;
     float m_duration = 1;
+    bool m_inverse = true;
 
 public:
-    TopDownWaveEffect(float duration);
+    TopDownWaveEffect(float duration, bool inverse);
     void fill(EffectBuffer& buffer, const EffectState& state) override;
 };
 
